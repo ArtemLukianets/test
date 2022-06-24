a894e035a7bfece15467807373e31a2cc5ef5fad
Artem <lykart007@gmail.com>
2022-06-24 10:13:30 +0300

    Change repositoria_2.py

--- repositoria_2.py
+++ repositoria_2.py
@@ -10,3 +10,11 @@
 my_list = [3, 2, 1, 7, 0, -3000.5845464, 25, -78, 45, 100, -1, 10]
 
 print(my_list[::-1])
+
+def hello():
+    for i in range(5):
+        print(str(i) + ": Hello world!")
+
+    print("-----")
+
+hello()
